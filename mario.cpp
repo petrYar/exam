f1ef42e3374ebb31940a35d50815a4361e4c3c1d
Petrashek Yaroslaw <yyy1505yyy1505@gmail.com>
2019-04-08 19:17:11 +0300

    enter sides

--- mario.cpp
+++ mario.cpp
@@ -3,12 +3,18 @@ using namespace std;
 int main()
 {
 	int heigth;
+	bool twoSides = false;
 
 	do
 	{
 		cout << "Enter heigth\n";
 		cin >> heigth;
 	} while (heigth <= 0);
+	do
+	{
+		cout << "Two sides? 1 - true 0 - false\n";
+		cin >> twoSides;
+	} while (twoSides < 0 || twoSides > 1);
 
 	system("pause");
 	return 0;
