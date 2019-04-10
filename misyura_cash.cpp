24791dfa5c74a26cfe7a734a967c9d05d937a63f
Petrashek Yaroslaw <yyy1505yyy1505@gmail.com>
2019-04-10 20:44:17 +0300

    Counting and showing coins

--- misyura_cash.cpp
+++ misyura_cash.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
+
+struct Nominals
+{
+	int c25 = 0;
+	int c10 = 0;
+	int c5 = 0;
+	int c1 = 0;
+};
+
 bool TakeNumber(int number);
 bool IsValid(int number);
 int ConvertToCoins(double number);
+void ShowTrifle(int coins, Nominals &nominals);
+
 int main()
 {
 	float number;
@@ -16,6 +27,14 @@ int main()
 	}
 
 	int coins = ConvertToCoins(number);
+
+	Nominals nominals;
+	ShowTrifle(coins, nominals);
+
+	cout << "Coins with 25 nominal: " << nominals.c25 << endl;
+	cout << "Coins with 10 nominal: " << nominals.c10 << endl;
+	cout << "Coins with 5 nominal: " << nominals.c5 << endl;
+	cout << "Coins with 1 nominal: " << nominals.c1 << endl;
 
 	system("pause");
 	return 0;
@@ -51,4 +70,28 @@ int ConvertToCoins(double number)
 	cout << "coins " << coins << endl;
 
 	return coins;
+}
+
+void ShowTrifle(int coins, Nominals &nominals)
+{
+	while (coins >= 25)
+	{
+		coins -= 25;
+		nominals.c25++;
+	}
+	while (coins >= 10)
+	{
+		coins -= 10;
+		nominals.c10++;
+	}
+	while (coins >= 5)
+	{
+		coins -= 5;
+		nominals.c5++;
+	}
+	while (coins >= 1)
+	{
+		coins -= 1;
+		nominals.c1++;
+	}
 }
